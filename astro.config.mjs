e9d90be233ea0bd643070495baedb83d8fbb0f58
ArthurSrn <arthsrnn@gmail.com>
2025-07-16 00:25:52 +0200

    Ajout de la prise en charge de React dans la configuration d'Astro, mise à jour des dépendances dans package.json, et amélioration des styles globaux avec Tailwind et tw-animate-css. Intégration de nouveaux composants dans les pages et mise à jour de la structure de NavBar.astro pour inclure des éléments visuels supplémentaires.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,26 +5,27 @@ import mkcert from 'vite-plugin-mkcert';
 import tailwindcss from "@tailwindcss/vite";
 
 
+import react from '@astrojs/react';
+
+
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 export default defineConfig({
-    integrations: [
-        storyblok({
-            accessToken: env.STORYBLOK_TOKEN,
-            components: {
-                page: "storyblok/Page",
-                grid: "storyblok/Grid",
-                feature: "storyblok/Feature",
-                teaser: "storyblok/Teaser",
-                navbar: "storyblok/Navbar",
+    integrations: [storyblok({
+        accessToken: env.STORYBLOK_TOKEN,
+        components: {
+            page: "storyblok/Page",
+            grid: "storyblok/Grid",
+            feature: "storyblok/Feature",
+            teaser: "storyblok/Teaser",
+            navbar: "storyblok/Navbar",
 
-            },
-            apiOptions: {
-                // Choisissez la région de votre espace Storyblok
-                region: 'eu', // facultatif,  ou 'eu' (par défaut)
-            },
-        })
-    ],
+        },
+        apiOptions: {
+            // Choisissez la région de votre espace Storyblok
+            region: 'eu', // facultatif,  ou 'eu' (par défaut)
+        },
+    }), react()],
     output: 'server',
     server: {
         port: 3000,
